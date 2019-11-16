7d0a3ad39f7b0bf1654aea9f9092379d624d028c
isonic1 <justin.ison@gmail.com>
2019-11-16 12:59:03 -0500

    updates for ios

--- lib/flick/applitools.rb
+++ lib/flick/applitools.rb
@@ -2,7 +2,7 @@ require 'date'
 
 class Applitoolz
 
-  attr_accessor :platform, :driver, :eyes, :app_name, :test_name, :keep
+  attr_accessor :platform, :driver, :eyes, :app_name, :test_name, :keep, :tag
 
   def initialize options
     self.platform = options[:platform]
@@ -13,19 +13,21 @@ class Applitoolz
     eyes.log_handler = Logger.new(STDOUT) if options[:showLogs]
     eyes.proxy = Applitools::Connectivity::Proxy.new options[:proxy] if options[:proxy] 
     eyes.match_level = options[:matchLevel].downcase.to_sym
-    eyes.baseline_name = options[:baseline]
+    eyes.baseline_env_name = options[:baseline]
     self.app_name = options[:appName]
     self.test_name = options[:testName]
     batch_info = Applitools::BatchInfo.new(options[:batchName])
-    batch_info.id = options[:batchId] #Date.today.to_time.to_i
+    batch_info.id = options[:batchId]
     eyes.batch = batch_info
     eyes.host_app = options[:hostApp]
     eyes.host_os = options[:hostOs]
     self.keep = options[:keep]
+    self.tag = options[:tag]
   end
   
   def upload_to_applitools
     image_info = driver.screenshot
+    tag = image_info[:udid] if tag.nil?
     eyes.test(app_name: app_name, test_name: test_name) do
       eyes.check_image(image_path: image_info[:path], tag: image_info[:udid])
       remove_image image_info[:path] unless keep
